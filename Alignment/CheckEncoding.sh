5c3549f4c92d328db597b9911f645202a80c9a04
BioInf2305 <58446112+BioInf2305@users.noreply.github.com>
2020-08-20 13:57:46 +0200

    Create CheckEncoding.sh
    Must be present in the same folder as the alignment python script

---
+++ Alignment/CheckEncoding.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+zcat $1|head -n 1000| awk '{if(NR%4==0) printf("%s",$0);}' |  od -A n -t u1 | awk 'BEGIN{min=100;max=0;}{for(i=1;i<=NF;i++) {if($i>max) max=$i; if($i<min) min=$i;}}END{if(max<=75 && min<59) print "Phred+33"; else if(max>73 && min>=64) print "Phred+64"; else if(min>=59 && min<64 && max>73) print "Solexa+64"; else print min,max, "Unknown score encoding!";}'
